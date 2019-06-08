8a455382f68c527314ef7d15d9ac0b612f4758a8
Jorge <jag7@outlook.es>
2019-06-07 19:29:57 -0500

    Add files via upload

---
+++ conversor.php
@@ -0,0 +1,20 @@
+<!--
+JAGProg
+Ejercicios para el aprendizaje
+-->
+
+
+<?php
+	
+	// Definimos la tasa de cambio
+	define ('TASA_DE_CAMBIO', 2800);
+	
+	//Obtenemos los dolares
+	$dolares = $_POST['dolares'];
+	// realiza la conversión y presenta el resultado
+	$pesos = $dolares * TASA_DE_CAMBIO;
+	echo "$dolares USD americanos son equivalentes a: $pesos COP";
+
+
+?>
+
